290071a144f61ec020307009c06b88957a94d68f
Sheerin Ibrahim <48320832+SheerinIbrahim@users.noreply.github.com>
2025-04-27 22:57:21 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-// Smooth scrolling for anchor links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+// Toggle navbar for mobile view
+document.getElementById("menu").onclick = function() {
+  document.querySelector("header .navbar").classList.toggle("nav-toggle");
+}
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
+// Optional: Smooth scroll for anchor links
+const links = document.querySelectorAll('a[href^="#"]');
+links.forEach(link => {
+  link.addEventListener('click', function(event) {
+    event.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
+  });
 });
+
+// Preloader Fadeout
+window.onload = () => {
+  setTimeout(() => {
+    document.querySelector(".loader-container").classList.add("fade-out");
+  }, 1500);
+}
